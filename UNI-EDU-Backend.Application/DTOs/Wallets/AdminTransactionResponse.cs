72c0358ca1a3c9a2a4ec69ad78700f7ef9f317fc
nguyenhuy-dev <nguyenhuy.se205@gmail.com>
2026-08-13 22:42:17 +0700

    [feat] add email attribute

--- UNI-EDU-Backend.Application/DTOs/Wallets/AdminTransactionResponse.cs
+++ UNI-EDU-Backend.Application/DTOs/Wallets/AdminTransactionResponse.cs
@@ -20,4 +20,5 @@ public class AdminTransactionResponse
 
     public string Description { get; set; } = string.Empty;
     public DateTime Date { get; set; }
+    public string Email { get; set; } = string.Empty;
 }
